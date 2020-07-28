bef3d2fe7487be57c0cfd2f0b313afb2c0db9f0b
Donald <donald.mate@gmail.com>
2020-07-28 15:29:20 -0400

    Added keyboard functionality, cleaned up logic

--- scripts.js
+++ scripts.js
@@ -22,10 +22,6 @@ function multiply (x, y){
     return x * y; 
 }
 function divide (x, y){
-    if (y = 0) {
-        removeChildNodes(screen);
-        screen.appendChild("Dividing by 0 is bad >:(");
-    }
     return x / y;
 }
 function operate (operator, x, y) {
@@ -66,92 +62,92 @@ function cleanUp() {
 }
 
 
-//BUTTON EVENT LISTENERS
-document.getElementById("0").addEventListener("click", function(){
+//INPUT FUNCTIONS
+function zeroPress () {
     buttonNumber = "0";
     currentNumber = createNumber(buttonNumber);
     displayNumber(currentNumber);
-});
-document.getElementById("1").addEventListener("click", function(){
+}
+function onePress () {
     buttonNumber = "1";
     currentNumber = createNumber(buttonNumber);
     displayNumber(currentNumber);
-});
-document.getElementById("2").addEventListener("click", function(){
+}
+function twoPress () {
     buttonNumber = "2";
     currentNumber = createNumber(buttonNumber);
     displayNumber(currentNumber);
-});
-document.getElementById("3").addEventListener("click", function(){
+}
+function threePress () {
     buttonNumber = "3";
     currentNumber = createNumber(buttonNumber);
     displayNumber(currentNumber);
-});
-document.getElementById("4").addEventListener("click", function(){
+}
+function fourPress () {
     buttonNumber = "4";
     currentNumber = createNumber(buttonNumber);
     displayNumber(currentNumber);
-});
-document.getElementById("5").addEventListener("click", function(){
+}
+function fivePress () {
     buttonNumber = "5";
     currentNumber = createNumber(buttonNumber);
     displayNumber(currentNumber);
-});
-document.getElementById("6").addEventListener("click", function(){
+}
+function sixPress () {
     buttonNumber = "6";
     currentNumber = createNumber(buttonNumber);
     displayNumber(currentNumber);
-});
-document.getElementById("7").addEventListener("click", function(){
+}
+function sevenPress () {
     buttonNumber = "7";
     currentNumber = createNumber(buttonNumber);
     displayNumber(currentNumber);
-});
-document.getElementById("8").addEventListener("click", function(){
+}
+function eightPress () {
     buttonNumber = "8";
     currentNumber = createNumber(buttonNumber);
     displayNumber(currentNumber);
-});
-document.getElementById("9").addEventListener("click", function(){
+}
+function ninePress () {
     buttonNumber = "9";
     currentNumber = createNumber(buttonNumber);
     displayNumber(currentNumber);
-});
-document.getElementById(".").addEventListener("click", function(){
+}
+function dotPress () {
     currentNumber = currentNumber + ".";
     displayNumber(currentNumber);
     document.getElementById(".").disabled = true;
-})
-document.getElementById("+").addEventListener("click", function(){
+}
+function plusPress () {
     finalNumber = operate(operator, lastNumber, currentNumber);
     operator = "+";
     displayNumber(finalNumber);
     cleanUp();
-});
-document.getElementById("-").addEventListener("click", function(){
+}
+function minusPress () {
     finalNumber = operate(operator, lastNumber, currentNumber);
     operator = "-";
     displayNumber(finalNumber);
     cleanUp();
-});
-document.getElementById("*").addEventListener("click", function(){
+}
+function timesPress () {
     finalNumber = operate(operator, lastNumber, currentNumber);
     operator = "*";
     displayNumber(finalNumber);
     cleanUp();
-});
-document.getElementById("/").addEventListener("click", function(){
+}
+function dividePress () {
     finalNumber = operate(operator, lastNumber, currentNumber);
     operator = "/";
     displayNumber(finalNumber);
     cleanUp();
-});
-document.getElementById("=").addEventListener("click", function(){
+}
+function equalsPress () {
     finalNumber = operate(operator, lastNumber, currentNumber);
     displayNumber(finalNumber);
     cleanUp();
-});
-document.getElementById("C").addEventListener("click", function(){
+}
+function clearPress () {
     removeChildNodes(screen);
     currentNumber = 0;
     lastNumber = 0;
@@ -159,19 +155,103 @@ document.getElementById("C").addEventListener("click", function(){
     operator = undefined;
     document.getElementById(".").disabled = false;
     first = true;
-})
-document.getElementById("back").addEventListener("click", function(){
+}
+function backPress () {
     let array = [];
     let buildingNumber = currentNumber.toString();
     for (i = 0; i < buildingNumber.length; i++) {
         array.push(buildingNumber.charAt(i));
     }
-    if (array[array.length - 1] = ".") {
+    if (array[array.length - 1] == ".") {
         document.getElementById(".").disabled = false;
     }
     array.pop();
     currentNumber = Number(array.join(""))
     displayNumber(currentNumber);
+}
+
+
+//BUTTON EVENT LISTENERS
+document.getElementById("0").addEventListener("click", function(){
+    zeroPress();
+});
+document.getElementById("1").addEventListener("click", function(){
+    onePress();
+});
+document.getElementById("2").addEventListener("click", function(){
+    twoPress();
+});
+document.getElementById("3").addEventListener("click", function(){
+    threePress();
+});
+document.getElementById("4").addEventListener("click", function(){
+    fourPress();
+});
+document.getElementById("5").addEventListener("click", function(){
+    fivePress();
+});
+document.getElementById("6").addEventListener("click", function(){
+    sixPress();
+});
+document.getElementById("7").addEventListener("click", function(){
+    sevenPress();
+});
+document.getElementById("8").addEventListener("click", function(){
+    eightPress();
+});
+document.getElementById("9").addEventListener("click", function(){
+    ninePress();
+});
+document.getElementById(".").addEventListener("click", function(){
+    dotPress();
+})
+document.getElementById("+").addEventListener("click", function(){
+    plusPress();
+});
+document.getElementById("-").addEventListener("click", function(){
+    minusPress();
+});
+document.getElementById("*").addEventListener("click", function(){
+    timesPress();
+});
+document.getElementById("/").addEventListener("click", function(){
+    dividePress();
+});
+document.getElementById("=").addEventListener("click", function(){
+    equalsPress();
+});
+document.getElementById("C").addEventListener("click", function(){
+    clearPress();
+})
+document.getElementById("back").addEventListener("click", function(){
+    backPress();
 })
 
-//To do: make division work, solve subtraction decimal issue
+
+//KEYBOARD SUPPORT
+document.onkeydown = function(e) {
+    if (e.which == 8) {
+        backPress();
+    }
+    let keyPressed = String.fromCharCode(e.which);
+    keyPressed === "0" ? zeroPress()
+    : keyPressed === "1" ? onePress()
+    : keyPressed === "2" ? twoPress()
+    : keyPressed === "3" ? threePress()
+    : keyPressed === "4" ? fourPress()
+    : keyPressed === "5" ? fivePress()
+    : keyPressed === "6" ? sixPress()
+    : keyPressed === "7" ? sevenPress()
+    : keyPressed === "8" ? eightPress()
+    : keyPressed === "9" ? ninePress()
+    : keyPressed === "." ? dotPress()
+    : keyPressed === "+" ? plusPress()
+    : keyPressed === "-" ? minusPress()
+    : keyPressed === "*" ? timesPress()
+    : keyPressed === "/" ? dividePress()
+    : keyPressed === "=" ? equalsPress()
+    : keyPressed === "c" ? clearPress()
+    : keyPressed = undefined;
+}
+
+//to do: fix division, decimal
