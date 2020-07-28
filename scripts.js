7866b1b0ddd4ee9e4e8d0bfc1961aa9f3456c061
Donald <donald.mate@gmail.com>
2020-07-28 14:15:18 -0400

    Added functionality for other numbers, equations, and decimals

--- scripts.js
+++ scripts.js
@@ -3,10 +3,14 @@ let buttonNumber;
 let currentNumber = 0;
 let lastNumber = 0;
 let display;
-let operator;
+let operator = undefined;
 let result;
 let finalNumber = 0;
+let first = true;
+const maxChars = 10;
 const screen = document.getElementById("screen");
+
+
 //MATHEMATICAL FUNCTIONS
 function add (x, y){
     return x + y;
@@ -19,18 +23,25 @@ function multiply (x, y){
 }
 function divide (x, y){
     if (y = 0) {
-        document.getElementById("screen").innerhtml = "Dividing by 0 is bad >:(";
+        removeChildNodes(screen);
+        screen.appendChild("Dividing by 0 is bad >:(");
     }
     return x / y;
 }
 function operate (operator, x, y) {
-    operator = "+" ?  result = add(x, y)
-    : operator = "-" ? result = subtract(x, y)
-    : operator = "*" ? result = multiply (x, y)
-    : operator = "/" ? result = divide (x, y)
-    : console.log("Uh oh! Something went wrong");
+    if (first == true) {
+        first = false;
+        return y;
+    }
+    operator === "+" ?  result = add(x, y)
+    : operator === "-" ? result = subtract(x, y)
+    : operator === "*" ? result = multiply (x, y)
+    : operator === "/" ? result = divide (x, y)
+    : removeChildNodes(screen) & screen.appendChild("Uh oh! :(");
     return result;
 }
+
+
 //OTHER KEY FUNCTIONS
 function removeChildNodes (parent) {
     while (parent.firstChild) {
@@ -38,43 +49,107 @@ function removeChildNodes (parent) {
     }
 }
 function createNumber (buttonNumber) {
+    if(currentNumber.toString().length > maxChars) {
+        return currentNumber;
+    }
     return Number(currentNumber.toString() + buttonNumber.toString());
 }
-
-
-
-
-//BUTTON EVENT LISTENERS
-document.getElementById("7").addEventListener("click", function(){
-    buttonNumber = "7";
-    currentNumber = createNumber(buttonNumber);
+function displayNumber (currentNumber) {
     display = document.createTextNode(currentNumber);
     removeChildNodes(screen);
     screen.appendChild(display);
+}
+function cleanUp() {
+    lastNumber = finalNumber;
+    currentNumber = 0;
+    document.getElementById(".").disabled = false;
+}
+
+
+//BUTTON EVENT LISTENERS
+document.getElementById("0").addEventListener("click", function(){
+    buttonNumber = "0";
+    currentNumber = createNumber(buttonNumber);
+    displayNumber(currentNumber);
+});
+document.getElementById("1").addEventListener("click", function(){
+    buttonNumber = "1";
+    currentNumber = createNumber(buttonNumber);
+    displayNumber(currentNumber);
+});
+document.getElementById("2").addEventListener("click", function(){
+    buttonNumber = "2";
+    currentNumber = createNumber(buttonNumber);
+    displayNumber(currentNumber);
+});
+document.getElementById("3").addEventListener("click", function(){
+    buttonNumber = "3";
+    currentNumber = createNumber(buttonNumber);
+    displayNumber(currentNumber);
+});
+document.getElementById("4").addEventListener("click", function(){
+    buttonNumber = "4";
+    currentNumber = createNumber(buttonNumber);
+    displayNumber(currentNumber);
 });
 document.getElementById("5").addEventListener("click", function(){
     buttonNumber = "5";
     currentNumber = createNumber(buttonNumber);
-    display = document.createTextNode(currentNumber);
-    removeChildNodes(screen);
-    screen.appendChild(display);
+    displayNumber(currentNumber);
 });
+document.getElementById("6").addEventListener("click", function(){
+    buttonNumber = "6";
+    currentNumber = createNumber(buttonNumber);
+    displayNumber(currentNumber);
+});
+document.getElementById("7").addEventListener("click", function(){
+    buttonNumber = "7";
+    currentNumber = createNumber(buttonNumber);
+    displayNumber(currentNumber);
+});
+document.getElementById("8").addEventListener("click", function(){
+    buttonNumber = "8";
+    currentNumber = createNumber(buttonNumber);
+    displayNumber(currentNumber);
+});
+document.getElementById("9").addEventListener("click", function(){
+    buttonNumber = "9";
+    currentNumber = createNumber(buttonNumber);
+    displayNumber(currentNumber);
+});
+document.getElementById(".").addEventListener("click", function(){
+    currentNumber = currentNumber + ".";
+    displayNumber(currentNumber);
+    document.getElementById(".").disabled = true;
+})
 document.getElementById("+").addEventListener("click", function(){
-    removeChildNodes(screen);
-    operator = "+";
     finalNumber = operate(operator, lastNumber, currentNumber);
-    display = document.createTextNode(finalNumber);
-    document.getElementById("screen").appendChild(display);
-    lastNumber = finalNumber;
-    currentNumber = 0;
+    operator = "+";
+    displayNumber(finalNumber);
+    cleanUp();
+});
+document.getElementById("-").addEventListener("click", function(){
+    finalNumber = operate(operator, lastNumber, currentNumber);
+    operator = "-";
+    displayNumber(finalNumber);
+    cleanUp();
+});
+document.getElementById("*").addEventListener("click", function(){
+    finalNumber = operate(operator, lastNumber, currentNumber);
+    operator = "*";
+    displayNumber(finalNumber);
+    cleanUp();
+});
+document.getElementById("/").addEventListener("click", function(){
+    finalNumber = operate(operator, lastNumber, currentNumber);
+    operator = "/";
+    displayNumber(finalNumber);
+    cleanUp();
 });
 document.getElementById("=").addEventListener("click", function(){
-    removeChildNodes(screen);
     finalNumber = operate(operator, lastNumber, currentNumber);
-    display = document.createTextNode(finalNumber);
-    document.getElementById("screen").appendChild(display);
-    lastNumber = 0;
-    currentNumber = finalNumber;
+    displayNumber(finalNumber);
+    cleanUp();
 });
 document.getElementById("C").addEventListener("click", function(){
     removeChildNodes(screen);
@@ -82,9 +157,8 @@ document.getElementById("C").addEventListener("click", function(){
     lastNumber = 0;
     finalNumber = 0;
     operator = undefined;
-})
-document.getElementById(".").addEventListener("click", function(){
-    
+    document.getElementById(".").disabled = false;
+    first = true;
 })
 document.getElementById("back").addEventListener("click", function(){
     let array = [];
@@ -92,12 +166,12 @@ document.getElementById("back").addEventListener("click", function(){
     for (i = 0; i < buildingNumber.length; i++) {
         array.push(buildingNumber.charAt(i));
     }
+    if (array[array.length - 1] = ".") {
+        document.getElementById(".").disabled = false;
+    }
     array.pop();
     currentNumber = Number(array.join(""))
-    display = document.createTextNode(currentNumber);
-    removeChildNodes(screen);
-    document.getElementById("screen").appendChild(display);
+    displayNumber(currentNumber);
 })
 
-
-// to do: decimals, prevent screen flooding, round long decimals to prevent screen flooding
+//To do: make division work, solve subtraction decimal issue
